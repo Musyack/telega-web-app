fe0ae9bb6af3f0b203539cdbab3e0ae3853d8696
fushy <fushingggslave@gmail.com>
2023-01-18 21:27:37 +0300

    11

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
             <Header/>
             <Routes>
                 <Route index element={<ProductList/>}/>
-                <Route path={'/form'} element={<Form/>}/>
+                <Route path={'form'} element={<Form/>}/>
             </Routes>
         </div>
     );
