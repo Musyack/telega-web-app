a3541183c38381c11102d6c15d145a6c43e8e531
fushy <fushingggslave@gmail.com>
2023-01-18 21:14:51 +0300

    11

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,7 +11,7 @@ const Form = () => {
 
     useEffect(() => {
         tg.MainButton.setParams({
-            text: 'Отправить данные!'
+            text: 'Отправить данные!!!'
         })
     }, [tg])
 
